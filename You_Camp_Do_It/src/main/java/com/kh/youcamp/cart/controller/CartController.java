b1342589b96588fefd2293babd232be93fb6ea74
Hong4848 <kgd1403@naver.com>
2024-12-17 18:09:58 +0900

    장바구니 프론트(24.12.17)
    log4j 설정, 장바구니 목록 조회 ajax 방식으로 수정, 장바구니 수량 업데이트 기능,
    프론트엔드 : 항목 체크에 따라 총 금액 변동
    +- 버튼에 따라 update ajax 요청

--- You_Camp_Do_It/src/main/java/com/kh/youcamp/cart/controller/CartController.java
+++ You_Camp_Do_It/src/main/java/com/kh/youcamp/cart/controller/CartController.java
@@ -1,8 +1,6 @@
 package com.kh.youcamp.cart.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -11,13 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
 import com.kh.youcamp.cart.model.service.CartService;
 import com.kh.youcamp.cart.model.vo.Cart;
 import com.kh.youcamp.member.model.vo.Member;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class CartController {
 	
 	@Autowired
@@ -26,37 +27,44 @@ public class CartController {
 	
 	/**
 	 * 24.12.06 10시 윤홍문작성
-	 * 장바구니 목록조회 뷰 접속 용 컨트롤러
+	 * 장바구니 목록조회 뷰 요청용 컨트롤러
 	 * @return
 	 */
-	@GetMapping("list.ca")
-	public ModelAndView selectList(ModelAndView mv,
-								   HttpSession session) {
-		
-		// 세션에서 유저 번호 가져오기
-		Member loginMember = 
-				(Member) session.getAttribute("loginMember");
-		int memberNo = 
-				(loginMember != null) ? loginMember.getMemberNo() : 0;
-//		System.out.println("장바구니 리스트뷰 컨트롤러 : " + memberNo);
-
-		// 로그인 인터셉터 처리 필요함@@@@@@@@@@@@@@@@@@@@@@@@@@
-		ArrayList<Cart> list = cartService.selectList(memberNo);
-				
-		mv.addObject("list", list);
-		mv.setViewName("cart/cartListView");
-//		System.out.println("카트 list 뷰 db 조회 후 컨트롤러 단" + list);
-		
-		 return mv;
+	@GetMapping("listForm.ca")
+	public String cartListViewForm() {
+		 return "cart/cartListView";
 	}
 	
 	/**
-	 * 장바구니 담기 요청 실행 메소드
+	 * 24.12.17 15시 윤홍문
+	 * 장바구니 리스트 조회용 컨트롤러
+	 * @param session
+	 * @return 회원번호기준 조회한 장바구니 목록
+	 */
+	@ResponseBody
+	@GetMapping(value="list.ca",
+				produces="application/json; charset=UTF-8")
+	public String ajaxSelectList(HttpSession session) {
+	    Member loginMember = (Member) session.getAttribute("loginMember");
+	    int memberNo = (loginMember != null) ? loginMember.getMemberNo() : 0;
+	    
+	    log.debug("장바구니 리스트뷰 컨트롤러 회원번호 : " + memberNo);
+
+	    ArrayList<Cart> list = cartService.selectList(memberNo);
+//	    log.debug("장바구니 AJAX 목록 조회 결과: " + list);
+
+	    return new Gson().toJson(list); // JSON 데이터 반환
+	}
+	
+	
+	/**
+	 * 장바구니 담기 요청 컨트롤러
 	 * @param cart
 	 */
 	@GetMapping("insert.ca")
 	public void insertCart(Cart cart) {
 		
+		// post 방식으로 처리 필요함@@@@@@@@@@@@@@@@@@@@@@@@
 		// 인터셉터 처리 필요함@@@@@@@@@@@@@@@@@@@@@@@@@@
 		// 로그인한 유저번호 세션에서 
 		// 장바구니 담기를 누른 페이지의 상품번호, 페이지에서 선택한 수량 쿼리스트링으로
@@ -86,7 +94,7 @@ public class CartController {
 				 produces="text/html; charset=UTF-8")
 	public String AjaxDeleteCart(String cartNos) {
 		
-		// System.out.println(cartNos);
+		log.debug("장바구니 삭제메소드 카트번호" + cartNos);
 		// null 또는 공백만있거나 또는 빈문자열"" 이면
 		// 빈배열 [] 할당
 		String[] cartNoList = 
@@ -100,6 +108,32 @@ public class CartController {
 		
 		return (result > 0) ? "success" : "fail";
 	}
+	
+	/**
+	 * 24.12.17 윤홍문작성
+	 * 장바구니 수량 업데이트 요청 메소드
+	 * @param cartNo 바꿀 장바구니 번호
+	 * @param quantity 바꿀 수량
+	 */
+	@ResponseBody
+	@PostMapping(value="updateQuantity.ca",
+				 produces="text/html; charset=UTF-8")
+	public String AjaxUpdateCartQuantity(Cart cart,
+										 HttpSession session) {
+		
+		Member loginMember = (Member) session.getAttribute("loginMember");
+	    int memberNo = (loginMember != null) ? loginMember.getMemberNo() : 0;
+		
+	    cart.setMemberNo(memberNo);
+		
+		log.debug("장바구니 수량업데이트 메소드, cart : " + cart);
+		
+		int result = cartService.updateCartQuantity(cart);
+		
+		return (result > 0) ? "success" : "fail";
+	}
+	
+	
 	
 	
 }
